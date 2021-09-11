823b82bbef228fa9d3cd28b1899ae6d9ef5e7db4
Galanyksa1@gmail.com <Galanyksa1@gmail.com>
2021-09-11 20:10:30 +0300

    Add forbid devide to zero

--- app.js
+++ app.js
@@ -1,40 +1,49 @@
 function calculator(){
-    let resultMessage = '';
+    
     let inputOperation = 0;
     let countOperands = 0;
     let numbers = [];
-    const mathOperations = ['+', '-', '/', '*'];
-    const operands = [2, 3, 4];
+
+    const MATH_OPERATIONS= ['+', '-', '/', '*'];
+    const OPERANDS = [2, 3, 4];
 
     function choiseOperation () {
         while(true) {
             inputOperation = prompt('Enter math operation');
-            if(mathOperations.includes(inputOperation)) {
+            if(MATH_OPERATIONS.includes(inputOperation)) {
             break;
             }
             alert('Wrong operation!!!');
         }
     }
+
     function numberOperants () {
         while (true)  {  
             countOperands = prompt('Enter number from 2 to 4');  
-            if(operands.includes(+countOperands)){
+            if(OPERANDS.includes(+countOperands)){
             break;
             }
             alert('Wrong number!!!');  
         }  
     }
+
     function inputOperants() {
         while(numbers.length < +countOperands) {
             let number = prompt('Enter number');
-            if((+number || +number === 0) && number !== '') {
-                numbers.push(number);
+            if((+number || +number === 0) && number !== ''){
+                if(inputOperation === '/' && +number === 0){
+                    alert('Wrong number!!!');  
+                }
+                else{
+                    numbers.push(number);
+                }  
             } 
             else{
                 alert('Wrong number!!!');  
             }
         }
     }
+
     function resultOperations() {
         let result = +numbers[0];
         let expression = numbers[0];
@@ -44,44 +53,36 @@ function calculator(){
            switch(inputOperation){
                 case'+': 
                     result += +numbers[index]; 
-                    operation = ' + ';
-                    expression += operation += numbers[index]; 
+                    expression += ' + ' + numbers[index]; 
                     ++index;
                 break;
+
                 case'-': 
                     result -= +numbers[index]; 
-                    operation = ' - ';
-                    expression += operation += numbers[index];
+                    expression += ' - ' + numbers[index];
                     ++index;
                 break;
+
                 case'*': 
                     result *= +numbers[index];
-                    operation = ' * ';
-                    expression += operation += numbers[index];
+                    expression += ' * ' + numbers[index];
                     ++index; 
                 break;
+
                 case'/': 
                     result /= +numbers[index];
-                    operation = ' / ';
-                    expression += operation += numbers[index]; 
+                    expression += ' / ' + numbers[index]; 
                     ++index;
                 break;
             }
         }
         alert(expression + ' = ' + result);
-        // if(index === 1) {
-        //     resultMessage += `${numbers[index-1]} ${inputOperation} ${numbers[index]}`;
-        // }else {
-        //     resultMessage += `${inputOperation} ${numbers[i]}`;
-        // }
-        // debugger
-        // // alert('result: ' + result);
-        // alert(`${resultMessage}=${result}`);
     }
+    
     choiseOperation();
     numberOperants();
     inputOperants();
     resultOperations();
 }
-calculator();
 
+calculator();
